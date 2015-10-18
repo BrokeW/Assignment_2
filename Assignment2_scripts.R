b015b95e6f96fc6c535ecf1c1fc976786a947783
YanW <Broke.w@gmail.com>
2015-10-17 22:22:23 -0400

    Finishing the table spliting

--- Assignment2_scripts.R
+++ Assignment2_scripts.R
@@ -1,2 +1,8 @@
 #Download the datasets
 T <- read.table("http://www.biostatisticien.eu/springeR/Weight_birth.csv", header=TRUE)
+#filter out the smoked mom
+pos.s <- which(T$SMOKE==1)
+T.S <- T[pos.s,]
+#filter out the non-smoked mom
+pos.ns <- which(T$SMOKE==0)
+T.NS <- T[pos.ns,]
