e34167a64dece830a2eef76c4dfc0678b94471d6
YanW <Broke.w@gmail.com>
2015-10-17 22:49:46 -0400

    1(d) finished

--- Assignment2_scripts.R
+++ Assignment2_scripts.R
@@ -10,3 +10,8 @@ T.NS <- T[pos.ns,]
 mean.weight.s <- mean(T.S[,"BWT"])
 mean.weight.ns <- mean(T.NS[,"BWT"])
 sprintf("The mean weight of children born to smoking mothers is mean.weight.s %s grams. The mean weight of children born to non-smoking mothers is mean.weight.ns %s grams.", mean.weight.s, mean.weight.ns)
+#filter out the mothers which have had no premature births in the past, and those had one or more
+pos.npre <- which(T$PTL==0)
+T.NP <- T[pos.npre,]
+pos.pre <- which(T$PTL>0)
+T.P <- T[pos.pre,]
