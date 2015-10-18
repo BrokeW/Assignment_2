3a98da496018f19c914452c4d9d65712ca48cfea
YanW <Broke.w@gmail.com>
2015-10-17 22:43:57 -0400

    1(c) done

--- Assignment2_scripts.R
+++ Assignment2_scripts.R
@@ -1,8 +1,12 @@
 #Download the datasets
 T <- read.table("http://www.biostatisticien.eu/springeR/Weight_birth.csv", header=TRUE)
-#filter out the smoked mom
+#filter out the smoking mom
 pos.s <- which(T$SMOKE==1)
 T.S <- T[pos.s,]
-#filter out the non-smoked mom
+#filter out the non-smoking mom
 pos.ns <- which(T$SMOKE==0)
 T.NS <- T[pos.ns,]
+#Print the mean birth weight of children born to smoking and non-smoking mothers separately
+mean.weight.s <- mean(T.S[,"BWT"])
+mean.weight.ns <- mean(T.NS[,"BWT"])
+sprintf("The mean weight of children born to smoking mothers is mean.weight.s %s grams. The mean weight of children born to non-smoking mothers is mean.weight.ns %s grams.", mean.weight.s, mean.weight.ns)
